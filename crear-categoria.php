5412213885af63b6dd014b66d46ea1b6621a0c77
Juanda <juandaalzate11@gmail.com>
2025-03-23 15:23:54 -0500

    Organice la visual en crear categorias y agregue estilos

--- crear-categoria.php
+++ crear-categoria.php
@@ -10,8 +10,10 @@ require_once ("includes/conexion.php");
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
+    <?php include 'includes/header.php'; ?>
+    <br>
     <h1>Crear nueva categoría</h1>
+    <br>
     
     <form action="guardar-categoria.php" method="POST">
         <label for="nombre">Nombre de la categoría:</label>
@@ -19,9 +21,9 @@ require_once ("includes/conexion.php");
         
         <input type="submit" value="Guardar">
     </form>
+    <button type="button" onclick="window.location.href='index.php'" class="boton boton-azul"> Volver al Inicio</button>
 
     <br>
-    <a href="index.php">Volver al inicio</a>
-
+    <?php include 'includes/footer.php'; ?>
 </body>
 </html>
--- libros/includes/footer.php
+++
@@ -1,7 +0,0 @@
-    <!-- PIE DE PÁGINA -->
-    <footer id="pie">
-        <p>Desarrollado por Luis Carlos Hernandez, Laura Catalina Rubio, Juan David Alzate, Sebastian Montoya &copy; 2025</p>
-    </footer>
-</div> <!-- fin contenedor -->
-</body>
-</html>
